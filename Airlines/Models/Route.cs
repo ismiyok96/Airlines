42ee6712839a056d7c1cda7634969ebdeb9e8e60
Mehmet Sıtkı ER <er.mehmet.96@outlook.com>
2018-11-06 12:42:23 +0000

    Update Airlines/Models/Route.cs

---
+++ Airlines/Models/Route.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airlines.Models
+{
+    public class Route
+    {
+        public string CountryName { get; set; }
+        public string CityName { get; set; }
+        public string AirportName { get; set; }
+        public int AirportID { get; set; }
+    }
+}
