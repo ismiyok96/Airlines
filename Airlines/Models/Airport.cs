8f020592fdf3ea40bdb4d3f855c32518711f51f3
Ömer <mazlumdincer@gmail.com>
2018-11-08 23:04:57 +0000

    Update Airlines/Models/Airport.cs
    Basic attributes about the airport which are we need the descripe an airport

---
+++ Airlines/Models/Airport.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Airlines.Models
+{
+    public class Airport
+        public string AirportName { get; set; } //Name of the Airport
+        public string Id { get; set; } = Guid.NewGuid().ToString(); //Unique Identifier of the Airport
+        public string AirportAbbre { get; set; } // Shortening of the Airport Name  
+
+    }
+}
